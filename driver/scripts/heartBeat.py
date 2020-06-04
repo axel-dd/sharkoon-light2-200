e3f866f7375ddc96ef6a701496c81f9173121d02
axel-dd <6698567+axel-dd@users.noreply.github.com>
2020-06-04 14:02:41 +0200

    remove c++ ui project add some scripts

--- driver/scripts/heartBeat.py
+++ driver/scripts/heartBeat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 ##
 ## This scripts helps to test the sysfs device attribute test
@@ -21,16 +21,18 @@ def main():
 
     test_file = test_files[0]
 
+    # Red Heartbeat LED Mode
+    data = b'\x04\xa0\x01\x02\x01\x02\xa5\x01\x07\x00\x08\x08\x00\x10\x10\x00\x18\x18\x00\x30\x30\x00\x40\x40\x00\x80\x80\x11\x40\x40\x00\x00\x00\x03\x02\x00\xa5\x08\x02\x0a\x01\x00\xff\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    if len(data) != 64:
+        print("wrong data size", file=sys.stderr)
+        sys.exit(1)
+
     # write to test file
     print("writing to " + test_file + " ...", file=sys.stdout) 
     with open(test_file, 'wb') as test_file_writer:
-        # test_file_writer.write(b'\x00\x00')
-        test_file_writer.write(b'\x04\xA0\x01\x01') # request the mouse settings
-        time.sleep(1)
-
-    # read test file (read response)
-    print("reading from " + test_file, file=sys.stdout) 
-    os.system("cat " + test_file)
+        test_file_writer.write(data)
 
 if __name__== "__main__":
     main()
+
+        
